3946335e2546e7729e88a3527e29a48212953537
Faress <bilal.faress@etu.univ-lyon1.fr>
2026-02-01 18:57:09 +0100

    carre cercle triangle

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 from torchvision import datasets, transforms
 import torch
-
+"""
 def get_dataloaders(batch_size=16):
     transform = transforms.Compose([
         transforms.Resize((64,64)),  #redimensionne l'image
@@ -11,4 +11,30 @@ def get_dataloaders(batch_size=16):
     return (
         torch.utils.data.DataLoader(train,batch_size,shuffle=True), #retourne les donnees en groupe et melanger
         torch.utils.data.DataLoader(val,batch_size) 
+    )"""
+
+
+def get_dataloaders(batch_size=16):
+    train_transform = transforms.Compose([     #pour l'entrainement
+        transforms.Resize((64,64)),            #redimensionne l'image
+        transforms.RandomRotation(360),        #apprend que l'orientation n'est pas importante
+        transforms.RandomHorizontalFlip(),     #evite au model d'apprendre des positions fixes
+        transforms.RandomVerticalFlip(),   
+        transforms.ToTensor(),                 #tenseurs doit etre fixe/normaliser
+        transforms.Normalize([0.5]*3, [0.5]*3) #permet d'avoir un gradient plus stable
+    ])
+
+    val_transform = transforms.Compose([       #pour la validation
+        transforms.Resize((64,64)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5]*3, [0.5]*3) #les valeurs doivent etre les memes qu'à l'entrainement
+    ])
+
+    train = datasets.ImageFolder("data/train", transform=train_transform) #retourne les donnees en groupe et melanger
+    val = datasets.ImageFolder("data/val", transform=val_transform)       #l'ordre alphabetiques des dossiers
+
+    return (
+        torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True), #retourne les donnees en groupe et melanger
+        torch.utils.data.DataLoader(val, batch_size=batch_size, shuffle=False)
     )
+
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 from model import SimpleCNN
 from dataset import get_dataloaders
 
+"""
 train_loader, _ = get_dataloaders() #recup les données en batch
 model = SimpleCNN()
 criterion = nn.CrossEntropyLoss() #fonction de perte
@@ -21,4 +22,26 @@ for epoch in range(10):
     print(f"Epoch {epoch+1} Loss {loss_sum:.3f}")
 
 torch.save(model.state_dict(),"human_cnn_v1.pth")
+print("Model saved")"""
+
+
+train_loader, val_loader = get_dataloaders() #recup les données en batch
+
+model = ShapeCNN(num_classes=3) #nombre de formes à reconnaitre
+criterion = nn.CrossEntropyLoss() #fonction de perte
+optimizer = optim.Adam(model.parameters(), lr=0.001) #maniere d'optimiser (Adam en l'occurence)
+
+#systeme qui ressemble a la fonction CheminNonSature (Dijkstra) dans le tp9 de lifapc dans l'actualisation des gradients
+for epoch in range(50):
+    model.train() #active le dropout
+    loss_sum = 0
+    for x,y in train_loader: #(x = image du batch ,y = etiquette [nom de dossier] correspondants)
+        optimizer.zero_grad() #reinitialise les gradients
+        loss = criterion(model(x), y) #calcul de perte
+        loss.backward() #calcul de gradient
+        optimizer.step() #maj des poids et biais selon les gradients
+        loss_sum += loss.item()
+    print(f"Epoch {epoch+1} Loss {loss_sum:.3f}")
+
+torch.save(model.state_dict(), "model.pth")
 print("Model saved")
